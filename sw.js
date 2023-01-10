1c9240307889dee8e93bf85d48e8738711c296a1
fernando-ui <fst-96@hotmail.com>
2023-01-09 21:28:43 -0600

    Comment the paths

--- sw.js
+++ sw.js
@@ -18,11 +18,11 @@ const APP_SHELL = [
   '/js/sw-utils.js'
 ];
 const APP_SHELL_INMUTABLE = [
-  "https://fonts.googleapis.com/css?family=Quicksand:300,400",
-  "https://fonts.googleapis.com/css?family=Lato:400,300",
-  "https://use.fontawesome.com/releases/v5.3.1/css/all.css",
-  "./css/animate.css",
-  "./js/libs/jquery.js",
+  "https://fonts.googleapis.com/css?family=Quicksand:300,400"
+  // "https://fonts.googleapis.com/css?family=Lato:400,300",
+  // "https://use.fontawesome.com/releases/v5.3.1/css/all.css",
+  // "./css/animate.css",
+  // "./js/libs/jquery.js",
 ];
 
 self.addEventListener("install", (e) => {
